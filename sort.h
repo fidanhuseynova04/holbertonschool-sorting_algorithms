4761734664d6a80c19c7aebcddf3bf46f38a9e54
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-04-15 12:38:32 +0000

    sort.h

--- sort.h
+++ sort.h
@@ -5,9 +5,10 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - function pointer for printing an array
- * @array: The array to be printed
- * @size: The number of elements in the array
+ * struct listint_s - 
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -16,8 +17,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-#endif /* SORT_H */
+#endif
