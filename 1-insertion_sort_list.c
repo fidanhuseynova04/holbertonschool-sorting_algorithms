37e33d0a3bb2ffdd6be67bbe01e80bbf3386a2d2
Qureyshi <kamranqureyshi1998@gmail.com>
2024-04-16 15:06:34 +0000

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "sort.h"
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
+ */
+void insertion_sort_list(listint_t **list)
+{
+
+}
