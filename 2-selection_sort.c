c605f6afcde443bc370de7f312b0861d1712c16d
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-04-21 13:26:29 +0000

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,10 +13,10 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min_index;
 	int tmp;
 
-	for (size_t i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		size_t min_index = i;
-		for (size_t j = i + 1; j < size; j++)
+		min_index = i;
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 			{
@@ -25,7 +25,7 @@ void selection_sort(int *array, size_t size)
 		}
 		if (min_index != i)
 		{
-			int temp = array[i];
+			temp = array[i];
 			array[i] = array[min_index];
 			array[min_index] = temp;
 			print_array(array, size);
